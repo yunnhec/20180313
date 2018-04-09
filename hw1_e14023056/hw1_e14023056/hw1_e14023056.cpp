2f6df8253127c660b7b991105b055c16c3344593
yunnhec <36875922+yunnhec@users.noreply.github.com>
2018-04-09 22:10:32 +0800

    20180409
    Implement of matrix multiplication

--- hw1_e14023056/hw1_e14023056/hw1_e14023056.cpp
+++ hw1_e14023056/hw1_e14023056/hw1_e14023056.cpp
@@ -9,53 +9,142 @@
 #include <windows.h>
 using namespace std;
 string ofname;
+
 class Matrix
 {
 public:
-	int **buildMatrix(int row, int column);
-	int **getMatrix() { return matrix; }
-	int multiplication();
-	void printA();
+	void initialize();	//pseudo-random number generator
+	int **buildMatrixA(int rowA, int columnA); //genetare a matrix with size (rowA, columnA)
+	int **buildMatrixX(int columnX); //genetare a matrix with size (columnA, columnX)
+	int **getMatrixA() { return matrixA; } //return matrixA
+	int **getMatrixX() { return matrixX; } //return matrixX
+	int **Multiplication(); //implement matrix multiplication and return a matrix of A*X
+	void print(); //print all result to screen and file
+	void printfile(); //print all result to file
+	void release(); //release memory
 private:
-	int i, j;
+	int i, j, k;
 	int size_Ar, size_Ac;
-	int size_xr, size_xc;
-	int **matrix;
+	int size_Xr, size_Xc;
+	int **matrixA, **matrixX, **matrixB;
 };
 
-int **Matrix::buildMatrix(int row, int column) {
-	size_Ar = row;
-	size_Ac = column;
-	matrix = new int*[row];
-	for (i = 0; i < row; i++)
-	{
-		matrix[i] = new int[column];
-	}
+void Matrix::initialize()
+{
 	srand(time(NULL));
+}
+
+int **Matrix::buildMatrixA(int rowA, int columnA) {
+	size_Ar = rowA;
+	size_Ac = columnA;
+
+	matrixA = new int*[size_Ar];
 	#pragma omp parallel for
-	for (i = 0; i < row; i++)
+	for (i = 0; i < size_Ar; i++)
+	{
+		matrixA[i] = new int[size_Ac];
+	}
+	
+	#pragma omp parallel for
+	for (i = 0; i < size_Ar; i++)
 	{
 	#pragma omp parallel for
-		for (j = 0; j < column; j++)
+		for (j = 0; j < size_Ac; j++)
 		{
-			matrix[i][j] = rand();
+			matrixA[i][j] = rand()%10; //all values are in 0~10
 		}	
 	}
-	return matrix;
+	return matrixA;
 }
-void Matrix::printA()
+
+int **Matrix::buildMatrixX(int columnX) {
+	int rowX = size_Ac;
+	size_Xr = rowX;
+	size_Xc = columnX;
+
+	matrixX = new int*[size_Xr];
+	#pragma omp parallel for
+	for (i = 0; i < size_Xr; i++)
+	{
+		matrixX[i] = new int[size_Xc];
+	}
+	#pragma omp parallel for
+	for (i = 0; i < size_Xr; i++)
+	{
+		#pragma omp parallel for
+		for (j = 0; j < size_Xc; j++)
+		{
+			matrixX[i][j] = rand()%10; //all values are in 0~10
+		}
+	}
+	return matrixX;
+}
+
+int **Matrix::Multiplication()
+{
+	matrixB = new int*[size_Ar];
+	#pragma omp parallel for
+	for (i = 0; i < size_Ar; i++)
+	{
+		matrixB[i] = new int[size_Xc];
+	}
+	#pragma omp parallel shared(matrixA,matrixB,matrixX) private(i,j,k)  
+	{
+		#pragma omp for schedule(dynamic)  
+		for (i = 0; i < size_Ar; i++)
+		{
+			for (j = 0; j < size_Xc; j++)
+			{
+				matrixB[i][j] = 0;
+				for (k = 0; k < size_Ac; k++)
+				{
+					matrixB[i][j] += matrixA[i][k] * matrixX[k][j];
+				}
+			}
+		}
+	}
+	return matrixB;
+}
+
+void Matrix::print()
 {
 	ofstream ofs(ofname, ios::out);
 	if (!ofs)
 		ofs << "Fail to open the ouput file." << endl;
 	else
 	{
+		cout << "matrix A =" << endl;
+		ofs << "A=[";
 		for (i = 0; i < size_Ar; i++)
 		{
 			for (j = 0; j < size_Ac; j++)
 			{
-				cout << matrix[i][j] << "\t";
-				ofs << matrix[i][j] << "\t";
+				cout << matrixA[i][j] << "\t";
+				ofs << matrixA[i][j] << "\t";
+			}
+			cout << endl;
+			ofs<< endl;
+		}
+		cout  << endl << "matrix X =" << endl;
+		ofs << "];" << endl << "X=[";
+		for (i = 0; i < size_Xr; i++)
+		{
+			for (j = 0; j < size_Xc; j++)
+			{
+				cout << matrixX[i][j] << "\t";
+				ofs << matrixX[i][j] << "\t";
+			}
+			cout << endl;
+			ofs << endl;
+		}
+		cout << endl << "A * X =" << endl;
+		ofs << "];" << endl << "A * X =" << endl;
+		for (i = 0; i < size_Ar; i++)
+		{
+			for (j = 0; j < size_Xc; j++)
+			{
+				cout << matrixB[i][j] << "\t";
+				ofs << matrixB[i][j] << "\t";
 			}
 			cout << endl;
 			ofs << endl;
@@ -63,91 +152,100 @@ void Matrix::printA()
 	}
 }
 
+void Matrix::printfile()
+{
+	ofstream ofs(ofname, ios::out);
+	if (!ofs)
+		ofs << "Fail to open the ouput file." << endl;
+	else
+	{
+		ofs << "A=[";
+		for (i = 0; i < size_Ar; i++)
+		{
+			for (j = 0; j < size_Ac; j++)
+			{
+				ofs << matrixA[i][j] << "\t";
+			}
+			ofs << endl;
+		}
+		ofs << "];" << endl << "X=[";
+		for (i = 0; i < size_Xr; i++)
+		{
+			for (j = 0; j < size_Xc; j++)
+			{
+				ofs << matrixX[i][j] << "\t";
+			}
+			ofs << endl;
+		}
+		ofs << "];" << endl << "A * X =" << endl;
+		for (i = 0; i < size_Ar; i++)
+		{
+			for (j = 0; j < size_Xc; j++)
+			{
+				ofs << matrixB[i][j] << "\t";
+			}
+			ofs << endl;
+		}
+	}
+}
+
+void Matrix::release()
+{
+	for (i = 0; i < size_Ar; i++)
+	{
+		delete[] matrixA[i];
+	}
+	delete[] matrixA;
+	for (i = 0; i < size_Xr; i++)
+	{
+		delete[] matrixX[i];
+	}
+	delete[] matrixX;
+	for (i = 0; i < size_Ar; i++)
+	{
+		delete[] matrixB[i];
+	}
+	delete[] matrixB;
+}
+
 int main()
 {
-	//clock_t tic, toc; //time_start and time_end
-	double T0, T1,T2;
-	int i, j;
-	int row = 10, column = 10;
-	int **matrix1, **matrix2;
+	clock_t tic, toc; //time_start and time_end
+	double T0, T1;
+	int i, j,k;
+	int row = 1000, column = 1000, column2 = 1;
+	int **A, **X, **B;
 	Matrix test;
 	ofname = "Result.txt";
-	matrix2 = test.buildMatrix(10,10);
-	test.printA();
-	
-	/*
+
 	tic = clock();
-	for (i = 0; i < row; i++)
+	#pragma omp parallel private(i,j,k)  
 	{
+	#pragma omp for schedule(dynamic)  
+		for (i = 0; i < row; i++){
+			for (j = 0; j < column2; j++){
+				for (k = 0; k < column; k++){
+				}
+			}
+		}
 	}
 	toc = clock();
 	T0 = (toc - tic) / (double)(CLOCKS_PER_SEC);
-	
+
+	test.initialize();
+	A = test.buildMatrixA(row,column);
+	X = test.buildMatrixX(column2);
+
 	tic = clock();
-	matrix1 = new int*[row];
-	for (i = 0; i < row; i++)
-	{
-		matrix1[i] = new int[column];
-	}
+	B = test.Multiplication();
 	toc = clock();
+
+	test.printfile();
+
 	T1 = (toc - tic) / (double)(CLOCKS_PER_SEC);
-	printf("settling time(wo/ omp) = \t%1f (s) \n", (T1 - T0) / 2.0);
+	printf("Operation time = %f\n" ,T1-T0);
+	test.release();
 
-	//settling data[row][column]
-	tic = clock();
-	matrix2 = new int*[row];
-	#pragma omp parallel for
-	for (i = 0; i < row; i++)
-	{
-		matrix2[i] = new int[column];
-	}
-	toc = clock();
-	T1 = (toc - tic) / (double)(CLOCKS_PER_SEC);
-	printf("settling time(w/ omp) = \t%1f (s) \n\n", (T1 - T0) / 2.0);
-
-	srand(time(NULL));
-
-	
-	tic = clock();
-	for (i = 0; i < row; i++)
-	{
-		for (j = 0; j < column; j++)
-		{
-			matrix1[i][j] = rand();
-			cout << matrix1[i][j] << "\t";
-		}
-		cout << endl;
-	}
-	toc = clock();
-	T2 = (toc - tic) / (double)(CLOCKS_PER_SEC);
-	printf("assignment time(wo/ omp) = \t%1f (s) \n", (T2 - T0) / 2.0);
-	
-	//data assignment
-	tic = clock();
-	#pragma omp parallel for
-		for (i = 0; i < row; i++)
-		{
-			#pragma omp parallel for
-			for (j = 0; j < column; j++)
-			{
-				matrix2[i][j] = rand();
-				cout << matrix2[i][j] << "\t";
-			}
-			cout << endl;
-		}
-		toc = clock();
-		T2 = (toc - tic) / (double)(CLOCKS_PER_SEC);
-		printf("assignment time(w/ omp) = \t%1f (s) \n\n", (T2 - T0) / 2.0);
-
-	//release
-		for (i = 0; i < row; i++)
-		{
-			delete[] matrix1[i];
-			delete[] matrix2[i];
-		}
-	delete[] matrix1;
-	delete[] matrix2;
-	*/
 	system("pause");
 	return 0;
 }
