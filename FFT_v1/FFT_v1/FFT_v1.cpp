7bc551d450785a40d08fc6ad3a0b9f9fb3e570aa
yunnhec <36875922+yunnhec@users.noreply.github.com>
2018-05-09 02:09:49 +0800

    20180509 02:10
    Bit Reverse for 2, 3, 5 are done!

--- FFT_v1/FFT_v1/FFT_v1.cpp
+++ FFT_v1/FFT_v1/FFT_v1.cpp
@@ -13,7 +13,9 @@ class FFT{
 private:
 	Complex<double> *in, *out;
 public:
-	int BitReverse(int num);
+	int BitReverse2(int num2);
+	int BitReverse3(int num3);
+	int BitReverse5(int num5);
 	void swap(int &a, int &b);
 	void test();
 };
@@ -25,8 +27,67 @@ void FFT::test(){
 	cout << "(x,y) = " << x << ", " << y << endl;
 }
 
-int FFT::BitReverse(int num){
+int FFT::BitReverse2(int num2){
+	int m, p, q, k;
+	m = num2 / 2;                        // Bit-Reverse 每次要進位的數字 
+	q = m;							// p = 1, q = m (第一個要交換的) 
+	for (p = 1; p<num2 - 1; ++p)
+	{
+		printf("%d <-> %d\n", p, q);
+		if (p < q)
+		{
+			//swap p and q
+		}
+		k = m;						// k, 用來檢查第 log_2 k + 1 位是不是1 
+		while (q >= k & k > 0)		// q >=k 第 (log_2 k + 1)位是1,  
+		{
+			q = q - k;				// 1->0
+			k = k / 2;				// 檢查下一位 
+		}
+		q = q + k;
+	}
+	return 0;
+}
 
+int FFT::BitReverse3(int num3){
+	int m, p, q, k;
+	m = num3 / 3;
+	q = m;
+	for (p = 1; p < num3-1; p++){
+		printf("%d <-> %d\n", p, q);
+		if (p < q)
+		{
+			//swap p and q
+		}
+		k = m;
+		while (q >= 2*k & k > 0)		// q >=k 第 (log_3 k + 1)位是2,  
+		{
+			q = q - 2*k;				// 2->0
+			k = k / 3;				// 檢查下一位 
+		}
+		q = q + k;
+	}
+	return 0;
+}
+
+int FFT::BitReverse5(int num5){
+	int m, p, q, k;
+	m = num5 / 5;
+	q = m;
+	for (p = 1; p < num5 - 1; p++){
+		printf("%d <-> %d\n", p, q);
+		if (p < q)
+		{
+			//swap p and q
+		}
+		k = m;
+		while (q >= 4 * k & k > 0)		// q >=k 第 (log_5 k + 1)位是4,  
+		{
+			q = q - 4 * k;				// 4->0
+			k = k / 5;				// 檢查下一位 
+		}
+		q = q + k;
+	}
 	return 0;
 }
 
@@ -40,7 +101,8 @@ void FFT::swap(int &a,int &b)
 int main()
 {
 	FFT t;
-	t.test();
+	//t.test();
+	t.BitReverse5(125);
 	cin.get();
 	//system("pause");
 	return 0;
