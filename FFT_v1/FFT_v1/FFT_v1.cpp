fbc7a0a20651efb117d097b899672b95cc152046
yunnhec <36875922+yunnhec@users.noreply.github.com>
2018-05-15 19:57:51 +0800

    20180515 19:58
    done

--- FFT_v1/FFT_v1/FFT_v1.cpp
+++ FFT_v1/FFT_v1/FFT_v1.cpp
@@ -11,7 +11,7 @@
 using namespace std;
 const int MaxNum = 100000000;//2147483647;
 const double M_PI = 3.141592653589;
-//ofstream ofs("Output.txt", ios::out);
+//ofstream ofs("Results.txt", ios::out);
 
 struct Complex{
 	double Real = 0.0;
@@ -27,22 +27,155 @@ private:
 	Complex *X;
 	int bitArray[50];
 	int p2=0, p3=0, p5=0;
-public:
 	void ini_Array(); //initialize bitArray
-	void swap(Complex &a, Complex &b);
 	void BitReverse();
+	void Butterfly();
 	void Butterfly2();
 	void Butterfly3();
 	void Butterfly5();
-	void getX(); 
+public:
 	void fft(int pow2, int pow3, int pow5);
+	void fft2(int Num);
+	void getX();
 };
+
+void FFT::fft2(int Num){
+	while (Num % 2 == 0){
+		p2++;
+		Num /= 2;
+	}
+	while (Num % 3 == 0){
+		p3++;
+		Num /= 3;
+	}
+	while (Num % 5 == 0){
+		p5++;
+		Num /= 5;
+	}
+	BitReverse();
+	Butterfly();
+}
 
 void FFT::fft(int pow2, int pow3, int pow5){
 	p2 = pow2;
 	p3 = pow3;
 	p5 = pow5;
 	BitReverse();
+	Butterfly();
+}
+
+void FFT::Butterfly(){
+	int k, p, q, r, s, t, c, m=1;
+	Complex w_N, w_2N, w_3N, w_4N, tmp1, tmp2, tmp3, tmp4;
+	int N = pow(2, p2)*pow(3, p3)*pow(5, p5);
+	double thetaN, theta;
+	for (c = 0; c < p5; c++){
+		m *= 5;
+		thetaN = -2.0*M_PI / 5;
+		w_N.Real = cos(thetaN);
+		w_N.Imag = sin(thetaN);
+		w_2N.Real = cos(2.0 * thetaN);
+		w_2N.Imag = sin(2.0 * thetaN);
+		w_3N.Real = cos(3.0 * thetaN);
+		w_3N.Imag = sin(3.0 * thetaN);
+		w_4N.Real = cos(4.0 * thetaN);
+		w_4N.Imag = sin(4.0 * thetaN);
+		for (k = 0; k < m / 5; k++)
+		{
+			theta = -2.0*k*M_PI / m;
+			for (p = k; p<N; p += m)
+			{
+				q = p + m / 5;
+				r = q + m / 5;
+				s = r + m / 5;
+				t = s + m / 5;
+				// (Complex)tmp1 = (Complex)w * (Complex)X[q]
+				tmp1.Real = cos(theta)*X[q].Real - sin(theta)*X[q].Imag;
+				tmp1.Imag = cos(theta)*X[q].Imag + sin(theta)*X[q].Real;
+				// (Complex)tmp2 = (Complex)w^2 * (Complex)X[r]
+				tmp2.Real = cos(2 * theta)*X[r].Real - sin(2 * theta)*X[r].Imag;
+				tmp2.Imag = cos(2 * theta)*X[r].Imag + sin(2 * theta)*X[r].Real;
+				// (Complex)tmp3 = (Complex)w^3 * (Complex)X[s]
+				tmp3.Real = cos(3 * theta)*X[s].Real - sin(3 * theta)*X[s].Imag;
+				tmp3.Imag = cos(3 * theta)*X[s].Imag + sin(3 * theta)*X[s].Real;
+				// (Complex)tmp4 = (Complex)w^4 * (Complex)X[t]
+				tmp4.Real = cos(4 * theta)*X[t].Real - sin(4 * theta)*X[t].Imag;
+				tmp4.Imag = cos(4 * theta)*X[t].Imag + sin(4 * theta)*X[t].Real;
+				// (Complex)X[q] = (Complex)X[p] + (Complex)tmp1*w_N + (Complex)tmp2*w_2N + (Complex)tmp3*w_3N + (Complex)tmp4*w_4N
+				X[q].Real = X[p].Real + (w_N.Real*tmp1.Real - w_N.Imag*tmp1.Imag) + (w_2N.Real*tmp2.Real - w_2N.Imag*tmp2.Imag) + (w_3N.Real*tmp3.Real - w_3N.Imag*tmp3.Imag) + (w_4N.Real*tmp4.Real - w_4N.Imag*tmp4.Imag);
+				X[q].Imag = X[p].Imag + (w_N.Real*tmp1.Imag + w_N.Imag*tmp1.Real) + (w_2N.Real*tmp2.Imag + w_2N.Imag*tmp2.Real) + (w_3N.Real*tmp3.Imag + w_3N.Imag*tmp3.Real) + (w_4N.Real*tmp4.Imag + w_4N.Imag*tmp4.Real);
+				// (Complex)X[r] = (Complex)X[p] + (Complex)tmp1*w_2N + (Complex)tmp2*w_4N + (Complex)tmp3*w_N + (Complex)tmp4*w_3N
+				X[r].Real = X[p].Real + (w_2N.Real*tmp1.Real - w_2N.Imag*tmp1.Imag) + (w_4N.Real*tmp2.Real - w_4N.Imag*tmp2.Imag) + (w_N.Real*tmp3.Real - w_N.Imag*tmp3.Imag) + (w_3N.Real*tmp4.Real - w_3N.Imag*tmp4.Imag);
+				X[r].Imag = X[p].Imag + (w_2N.Real*tmp1.Imag + w_2N.Imag*tmp1.Real) + (w_4N.Real*tmp2.Imag + w_4N.Imag*tmp2.Real) + (w_N.Real*tmp3.Imag + w_N.Imag*tmp3.Real) + (w_3N.Real*tmp4.Imag + w_3N.Imag*tmp4.Real);
+				// (Complex)X[s] = (Complex)X[p] + (Complex)tmp1*w_3N + (Complex)tmp2*w_N + (Complex)tmp3*w_4N + (Complex)tmp4*w_2N
+				X[s].Real = X[p].Real + (w_3N.Real*tmp1.Real - w_3N.Imag*tmp1.Imag) + (w_N.Real*tmp2.Real - w_N.Imag*tmp2.Imag) + (w_4N.Real*tmp3.Real - w_4N.Imag*tmp3.Imag) + (w_2N.Real*tmp4.Real - w_2N.Imag*tmp4.Imag);
+				X[s].Imag = X[p].Imag + (w_3N.Real*tmp1.Imag + w_3N.Imag*tmp1.Real) + (w_N.Real*tmp2.Imag + w_N.Imag*tmp2.Real) + (w_4N.Real*tmp3.Imag + w_4N.Imag*tmp3.Real) + (w_2N.Real*tmp4.Imag + w_2N.Imag*tmp4.Real);
+				// (Complex)X[t] = (Complex)X[p] + (Complex)tmp1*w_4N + (Complex)tmp2*w_3N + (Complex)tmp3*w_2N + (Complex)tmp4*w_N
+				X[t].Real = X[p].Real + (w_4N.Real*tmp1.Real - w_4N.Imag*tmp1.Imag) + (w_3N.Real*tmp2.Real - w_3N.Imag*tmp2.Imag) + (w_2N.Real*tmp3.Real - w_2N.Imag*tmp3.Imag) + (w_N.Real*tmp4.Real - w_N.Imag*tmp4.Imag);
+				X[t].Imag = X[p].Imag + (w_4N.Real*tmp1.Imag + w_4N.Imag*tmp1.Real) + (w_3N.Real*tmp2.Imag + w_3N.Imag*tmp2.Real) + (w_2N.Real*tmp3.Imag + w_2N.Imag*tmp3.Real) + (w_N.Real*tmp4.Imag + w_N.Imag*tmp4.Real);
+				// (Complex)X[p] = (Complex)X[p] + (Complex)tmp1 + (Complex)tmp2
+				X[p].Real += tmp1.Real + tmp2.Real + tmp3.Real + tmp4.Real;
+				X[p].Imag += tmp1.Imag + tmp2.Imag + tmp3.Imag + tmp4.Imag;
+			}
+		}
+	}
+	for (c = 0; c < p3; c++){
+		m *= 3;
+		thetaN = -2.0*M_PI / 3;
+		w_N.Real = cos(thetaN);
+		w_N.Imag = sin(thetaN);
+		w_2N.Real = cos(2 * thetaN);
+		w_2N.Imag = sin(2 * thetaN);
+		for (k = 0; k < m / 3; k++)
+		{
+			theta = -2.0*k*M_PI / m;
+			for (p = k; p<N; p += m)
+			{
+				q = p + m / 3;
+				r = q + m / 3;
+				// (Complex)tmp1 = (Complex)w * (Complex)X[q]
+				tmp1.Real = cos(theta)*X[q].Real - sin(theta)*X[q].Imag;
+				tmp1.Imag = cos(theta)*X[q].Imag + sin(theta)*X[q].Real;
+				// (Complex)tmp2 = (Complex)w^2 * (Complex)X[r]
+				tmp2.Real = cos(2 * theta)*X[r].Real - sin(2 * theta)*X[r].Imag;
+				tmp2.Imag = cos(2 * theta)*X[r].Imag + sin(2 * theta)*X[r].Real;
+				// (Complex)X[r] = (Complex)X[p] + (Complex)tmp1*w_N^2 + (Complex)tmp2*w_N^4
+				//							   = (Complex)X[p] + (Complex)tmp1*w_2N + (Complex)tmp2*w_N
+				X[r].Real = X[p].Real + (w_2N.Real*tmp1.Real - w_2N.Imag*tmp1.Imag) + (w_N.Real*tmp2.Real - w_N.Imag*tmp2.Imag);
+				X[r].Imag = X[p].Imag + (w_2N.Real*tmp1.Imag + w_2N.Imag*tmp1.Real) + (w_N.Real*tmp2.Imag + w_N.Imag*tmp2.Real);
+				// (Complex)X[q] = (Complex)X[p] + (Complex)tmp1*w_N + (Complex)tmp2*w_N^2
+				//                             = (Complex)X[p] + (Complex)tmp1*w_N + (Complex)tmp2*w_2N
+				X[q].Real = X[p].Real + (w_N.Real*tmp1.Real - w_N.Imag*tmp1.Imag) + (w_2N.Real*tmp2.Real - w_2N.Imag*tmp2.Imag);
+				X[q].Imag = X[p].Imag + (w_N.Real*tmp1.Imag + w_N.Imag*tmp1.Real) + (w_2N.Real*tmp2.Imag + w_2N.Imag*tmp2.Real);
+				// (Complex)X[p] = (Complex)X[p] + (Complex)tmp1 + (Complex)tmp2
+				X[p].Real += tmp1.Real + tmp2.Real;
+				X[p].Imag += tmp1.Imag + tmp2.Imag;
+			}
+		}
+	}
+	for (c = 0; c < p2; c++){
+		m *= 2;
+		thetaN = -2.0*M_PI / 2;
+		w_N.Real = cos(thetaN);
+		w_N.Imag = sin(thetaN);
+		for (k = 0; k < m / 2; k++)
+		{
+			theta = -2.0*k*M_PI / m;
+			for (p = k; p<N; p += m)
+			{
+				q = p + m / 2;
+				// (Complex)tmp = (Complex)w * (Complex)X[q]
+				tmp1.Real = cos(theta)*X[q].Real - sin(theta)*X[q].Imag;
+				tmp1.Imag = cos(theta)*X[q].Imag + sin(theta)*X[q].Real;
+				// (Complex)X[q] = (Complex)X[p] - (Complex)tmp
+				X[q].Real = X[p].Real - tmp1.Real;
+				X[q].Imag = X[p].Imag - tmp1.Imag;
+				// (Complex)X[p] = (Complex)X[p] + (Complex)tmp
+				X[p].Real += tmp1.Real;
+				X[p].Imag += tmp1.Imag;
+			}
+		}
+	}
 }
 
 void FFT::Butterfly2(){
@@ -53,8 +186,8 @@ void FFT::Butterfly2(){
 	{
 		for (k = 0; k<m / 2; k++)
 		{
-			w.Real = cos(2.0*k*M_PI / m); // -????????? 
-			w.Imag = sin(2.0*k*M_PI / m);
+			w.Real = cos(-2.0*k*M_PI / m);
+			w.Imag = sin(-2.0*k*M_PI / m);
 			for (p = k; p<N; p += m)
 			{
 				q = p + m / 2;
@@ -77,16 +210,16 @@ void FFT::Butterfly3(){
 	int k, p, q, r, m = 3;
 	Complex w, w_N, w_2N, tmp1, tmp2;
 	int N = pow(2, p2)*pow(3, p3)*pow(5, p5);
-	w_N.Real = cos(2.0*M_PI / 3);
-	w_N.Imag = sin(2.0*M_PI / 3);
-	w_2N.Real = cos(2.0*2*M_PI / 3);
-	w_2N.Imag = sin(2.0*2*M_PI / 3);
+	w_N.Real = cos(-2.0*M_PI / 3);
+	w_N.Imag = sin(-2.0*M_PI / 3);
+	w_2N.Real = cos(-2.0*2*M_PI / 3);
+	w_2N.Imag = sin(-2.0*2*M_PI / 3);
 	while (m <= N)
 	{
 		for (k = 0; k<m / 3; k++)
 		{
-			w.Real = cos(2.0*k*M_PI / m);
-			w.Imag = sin(2.0*k*M_PI / m);
+			w.Real = cos(-2.0*k*M_PI / m);
+			w.Imag = sin(-2.0*k*M_PI / m);
 			for (p = k; p<N; p += m)
 			{
 				q = p + m / 3;
@@ -97,7 +230,6 @@ void FFT::Butterfly3(){
 				// (Complex)tmp2 = (Complex)w^2 * (Complex)X[r]
 				tmp2.Real = (w.Real *w.Real - w.Imag * w.Imag)*X[r].Real - (2 * w.Real*w.Imag)*X[r].Imag;
 				tmp2.Imag = (w.Real *w.Real - w.Imag * w.Imag)*X[r].Imag + (2 * w.Real*w.Imag)*X[r].Real;
-				
 				// (Complex)X[r] = (Complex)X[p] + (Complex)tmp1*w_N^2 + (Complex)tmp2*w_N^4
 				//							   = (Complex)X[p] + (Complex)tmp1*w_2N + (Complex)tmp2*w_N
 				X[r].Real = X[p].Real + (w_2N.Real*tmp1.Real - w_2N.Imag*tmp1.Imag) + (w_N.Real*tmp2.Real - w_N.Imag*tmp2.Imag);
@@ -109,20 +241,6 @@ void FFT::Butterfly3(){
 				// (Complex)X[p] = (Complex)X[p] + (Complex)tmp1 + (Complex)tmp2
 				X[p].Real += tmp1.Real + tmp2.Real;
 				X[p].Imag += tmp1.Imag + tmp2.Imag;
-				
-				/*
-				// (Complex)X[r] = (Complex)X[p] + (Complex)tmp1*w_N^2 + (Complex)tmp2*w_N^4
-				//							   = (Complex)X[p] + (Complex)tmp1*w_2N + (Complex)tmp2*w_N
-				X[r].Real = X[p].Real + w_N.Real*(tmp1.Real + tmp2.Real) + w_N.Imag*(tmp1.Imag - tmp2.Imag);
-				X[r].Imag = X[p].Imag + w_N.Real*(tmp1.Imag + tmp2.Imag) - w_N.Imag*(tmp1.Real - tmp2.Real);
-				// (Complex)X[q] = (Complex)X[p] + (Complex)tmp1*w_N + (Complex)tmp2*w_N^2
-				//                             = (Complex)X[p] + (Complex)tmp1*w_N + (Complex)tmp2*w_2N
-				X[q].Real = X[p].Real + w_N.Real*(tmp1.Real + tmp2.Real) - w_N.Imag*(tmp1.Imag - tmp2.Imag);
-				X[q].Imag = X[p].Imag + w_N.Real*(tmp1.Imag + tmp2.Imag) + w_N.Imag*(tmp1.Real - tmp2.Real);
-				// (Complex)X[p] = (Complex)X[p] + (Complex)tmp1 + (Complex)tmp2
-				X[p].Real += tmp1.Real + tmp2.Real;
-				X[p].Imag += tmp1.Imag + tmp2.Imag;
-				*/
 			}
 		}
 		m *=3;
@@ -133,22 +251,23 @@ void FFT::Butterfly5(){
 	int k, p, q, r, s, t, m = 5;
 	Complex w, w_N, w_2N, w_3N, w_4N, tmp1, tmp2, tmp3, tmp4;
 	int N = pow(2, p2)*pow(3, p3)*pow(5, p5);
-	double theta = 2.0*M_PI / 5;
-	w_N.Real = cos(theta);
-	w_N.Imag = sin(theta);
-	w_2N.Real = cos(2.0 * theta);
-	w_2N.Imag = sin(2.0 * theta);
-	w_3N.Real = cos(3.0 * theta);
-	w_3N.Imag = sin(3.0 * theta);
-	w_4N.Real = cos(4.0 * theta);
-	w_4N.Imag = sin(4.0 * theta);
+	double thetaN = -2.0*M_PI / 5,theta;
+	w_N.Real = cos(thetaN);
+	w_N.Imag = sin(thetaN);
+	w_2N.Real = cos(2.0 * thetaN);
+	w_2N.Imag = sin(2.0 * thetaN);
+	w_3N.Real = cos(3.0 * thetaN);
+	w_3N.Imag = sin(3.0 * thetaN);
+	w_4N.Real = cos(4.0 * thetaN);
+	w_4N.Imag = sin(4.0 * thetaN);
 
 	while (m <= N)
 	{
 		for (k = 0; k<m / 5; k++)
 		{
-			w.Real = cos(2.0*k*M_PI / m);
-			w.Imag = sin(2.0*k*M_PI / m);
+			theta = -2.0*k*M_PI / m;
+			//w.Real = cos(2.0*k*M_PI / m);
+			//w.Imag = sin(2.0*k*M_PI / m);
 			for (p = k; p<N; p += m)
 			{
 				q = p + m / 5;
@@ -156,18 +275,17 @@ void FFT::Butterfly5(){
 				s = r + m / 5;
 				t = s + m / 5;
 				// (Complex)tmp1 = (Complex)w * (Complex)X[q]
-				tmp1.Real = w.Real*X[q].Real - w.Imag*X[q].Imag;
-				tmp1.Imag = w.Real*X[q].Imag + w.Imag*X[q].Real;
+				tmp1.Real = cos(theta)*X[q].Real - sin(theta)*X[q].Imag;
+				tmp1.Imag = cos(theta)*X[q].Imag + sin(theta)*X[q].Real;
 				// (Complex)tmp2 = (Complex)w^2 * (Complex)X[r]
-				tmp2.Real = (w.Real *w.Real - w.Imag * w.Imag)*X[r].Real - (2 * w.Real*w.Imag)*X[r].Imag;
-				tmp2.Imag = (w.Real *w.Real - w.Imag * w.Imag)*X[r].Imag + (2 * w.Real*w.Imag)*X[r].Real;
+				tmp2.Real = cos(2 * theta)*X[r].Real - sin(2 * theta)*X[r].Imag;
+				tmp2.Imag = cos(2 * theta)*X[r].Imag + sin(2 * theta)*X[r].Real;
 				// (Complex)tmp3 = (Complex)w^3 * (Complex)X[s]
-				tmp3.Real = (w.Real*w.Real*w.Real - 3 * w.Real*w.Imag*w.Imag)*X[s].Real - (w.Imag*w.Imag*w.Imag - 3 * w.Real*w.Real*w.Imag)*X[s].Imag;
-				tmp3.Imag = (w.Real*w.Real*w.Real - 3 * w.Real*w.Imag*w.Imag)*X[s].Imag + (w.Imag*w.Imag*w.Imag - 3 * w.Real*w.Real*w.Imag)*X[s].Real;
+				tmp3.Real = cos(3 * theta)*X[s].Real - sin(3 * theta)*X[s].Imag;
+				tmp3.Imag = cos(3 * theta)*X[s].Imag + sin(3 * theta)*X[s].Real;
 				// (Complex)tmp4 = (Complex)w^4 * (Complex)X[t]
-				tmp4.Real = (w.Real*w.Real*w.Real*w.Real - 6 * w.Real*w.Imag*w.Real*w.Imag + w.Imag*w.Imag*w.Imag*w.Imag)*X[t].Real - (4 * w.Real*w.Real*w.Real*w.Imag - 4 * w.Real*w.Imag*w.Imag*w.Imag)*X[t].Imag;
-				tmp4.Imag = (w.Real*w.Real*w.Real*w.Real - 6 * w.Real*w.Imag*w.Real*w.Imag + w.Imag*w.Imag*w.Imag*w.Imag)*X[t].Imag + (4 * w.Real*w.Real*w.Real*w.Imag - 4 * w.Real*w.Imag*w.Imag*w.Imag)*X[t].Real;
-				//*************
+				tmp4.Real = cos(4 * theta)*X[t].Real - sin(4 * theta)*X[t].Imag;
+				tmp4.Imag = cos(4 * theta)*X[t].Imag + sin(4 * theta)*X[t].Real;
 				// (Complex)X[q] = (Complex)X[p] + (Complex)tmp1*w_N + (Complex)tmp2*w_2N + (Complex)tmp3*w_3N + (Complex)tmp4*w_4N
 				X[q].Real = X[p].Real + (w_N.Real*tmp1.Real - w_N.Imag*tmp1.Imag) + (w_2N.Real*tmp2.Real - w_2N.Imag*tmp2.Imag) + (w_3N.Real*tmp3.Real - w_3N.Imag*tmp3.Imag) + (w_4N.Real*tmp4.Real - w_4N.Imag*tmp4.Imag);
 				X[q].Imag = X[p].Imag + (w_N.Real*tmp1.Imag + w_N.Imag*tmp1.Real) + (w_2N.Real*tmp2.Imag + w_2N.Imag*tmp2.Real) + (w_3N.Real*tmp3.Imag + w_3N.Imag*tmp3.Real) + (w_4N.Real*tmp4.Imag + w_4N.Imag*tmp4.Real);
@@ -180,7 +298,6 @@ void FFT::Butterfly5(){
 				// (Complex)X[t] = (Complex)X[p] + (Complex)tmp1*w_4N + (Complex)tmp2*w_3N + (Complex)tmp3*w_2N + (Complex)tmp4*w_N
 				X[t].Real = X[p].Real + (w_4N.Real*tmp1.Real - w_4N.Imag*tmp1.Imag) + (w_3N.Real*tmp2.Real - w_3N.Imag*tmp2.Imag) + (w_2N.Real*tmp3.Real - w_2N.Imag*tmp3.Imag) + (w_N.Real*tmp4.Real - w_N.Imag*tmp4.Imag);
 				X[t].Imag = X[p].Imag + (w_4N.Real*tmp1.Imag + w_4N.Imag*tmp1.Real) + (w_3N.Real*tmp2.Imag + w_3N.Imag*tmp2.Real) + (w_2N.Real*tmp3.Imag + w_2N.Imag*tmp3.Real) + (w_N.Real*tmp4.Imag + w_N.Imag*tmp4.Real);
-				//*************
 				// (Complex)X[p] = (Complex)X[p] + (Complex)tmp1 + (Complex)tmp2
 				X[p].Real += tmp1.Real + tmp2.Real + tmp3.Real + tmp4.Real;
 				X[p].Imag += tmp1.Imag + tmp2.Imag + tmp3.Imag + tmp4.Imag;
@@ -225,15 +342,9 @@ void FFT::BitReverse(){
 	X[N - 1].Real = (double)(N - 1)*1.0;
 }
 
-void FFT::swap(Complex &a, Complex &b)
-{
-	Complex tmp = a;
-	a = b;
-	b = tmp;
-}
-
 void FFT::getX(){
-	for (int i = 0; i < pow(2, p2)*pow(3, p3)*pow(5, p5); i++){
+	int i;
+	for (i = 0; i < pow(2, p2)*pow(3, p3)*pow(5, p5); i++){
 		X[i].data();
 		cout << endl;
 	}
@@ -243,13 +354,41 @@ int main()
 {
 	clock_t t1, t2;
 	FFT t;
-	cout << "BitReverse" << endl;
+	int a, b, c, N, Icase;
+	cout << "Input (1) power or (2) number? ";
+	cin >> Icase;
+	switch (Icase){
+	case 1:
+		cout << "power of 2: ";
+		cin >> a;
+		cout << "power of 3: ";
+		cin >> b;
+		cout << "power of 5: ";
+		cin >> c;
+		t1 = clock();
+		t.fft(a, b, c);
+		t2 = clock();
+		cout << "FFT of N = 0 ~ " << pow(2, a)*pow(3, b)*pow(5, c) << endl;
+		break;
+	case 2:
+		cout << "Input the number: ";
+		cin >> N;
+		t1 = clock();
+		t.fft2(N);
+		t2 = clock();
+		cout << "FFT of N = 0 ~ " << N << endl;
+		break;
+	default:
+		break;
+	}
+	
+	/*
 	t1 = clock();
-	t.fft(0,2,0);
+	t.fft(a, b, c);
+	//t.fft2(360);
 	t2 = clock();
-	//t.getX();
-	t.Butterfly3();
-	cout << "FFT" << endl;
+	*/
+	
 	t.getX();
 	printf("time = %f\n", (t2 - t1) / (double)(CLOCKS_PER_SEC));
 	system("pause");
